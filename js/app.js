96a39b648ce4e2d647cd803ef054c87bbab2e854
Jo Wightman <jo.killercases@gmail.com>
2013-09-21 18:01:58 -0400

    Jo

--- js/app.js
+++ js/app.js
@@ -1,9 +1,6 @@
 $(document).ready(function(e){
-	
-//Get the context of the canvas element we want to select
-var ctx = document.getElementById("myChart").getContext("2d");
-var myNewChart = new Chart(ctx).PolarArea(data);
-new Chart(ctx).PolarArea(data,options);
+
+
 
 
 });
